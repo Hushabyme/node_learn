6d850c5f859e4e817704ba0beea05a4b4cdce889
1065432936 <1065432936@qq.com>
2017-03-24 18:34:57 +0800

    changes

--- Node.js 权威指南/01 - 第一章/01 - firstNode.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-http.createServer((request, response) => {
-  response.writeHead(200, {'Content-type': 'text/html'});
-  response.write('<head><meta charset="utf-8"></head>');
-  response.end('<h1>Hello World! I\'m learning Node.js.</h1>');
-}).listen(8080, '127.0.0.1');
-
-console.log('Server running at http://localhost:8080');
